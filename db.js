8a784dad15949a70faa54e7f1e7e6c67d818a669
Alejandro Navarro <drojannp@gmail.com>
2023-01-25 14:00:11 +0100

    Instalo detenv y creo archivo

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const {createPool} = require('mysql2/promise')
 const pool =  createPool({
     host: 'localhost',
     user: 'root',
-    password:'alejandro',
+    password: 'alejandro',
     port: 3306,
     database: 'userdb'
 })
